58bf9a15afb5b43cde475cee4a41afbd83227201
Soojin6943 <sjhan0711@gmail.com>
2025-10-23 17:04:52 +0900

    feat(inputview): 자동차 이름 입력 받기

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,11 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+
+        String carsName = inputView.readCarName();
+
+        // 임시 실행 테스트
+        System.out.println(carsName);
     }
 }
