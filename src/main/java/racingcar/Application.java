ec47e3a3f24d3d1bfc09269db2df4e0633539a5b
Soojin6943 <sjhan0711@gmail.com>
2025-10-23 22:50:48 +0900

    feat(outputview): 우승자 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -7,12 +7,18 @@ public class Application {
         InputView inputView = new InputView();
         Cars cars = new Cars();
         RacingGame racingGame = new RacingGame();
+        OutputView outputView = new OutputView();
 
         String carsName = inputView.readCarName();
         int tryCount = inputView.readTryCount();
 
+        System.out.println("실행 결과");
         List<Car> carList = cars.carParser(carsName);
 
         racingGame.startRace(tryCount, carList);
+
+        int maxPosition = cars.maxPosition(carList);
+        List<String> winners = cars.winner(carList, maxPosition);
+        outputView.printWinner(winners);
     }
 }
--- src/main/java/racingcar/OutputView.java
+++ src/main/java/racingcar/OutputView.java
@@ -1,8 +1,17 @@
 package racingcar;
 
+import java.util.List;
+
 public class OutputView {
     // 게임 진행 중 출력
     public void printCarPosition(Car car) {
         System.out.println(car);
+    }
+
+    // 우승자 출력
+    public void printWinner(List<String> winners){
+        String winnerMessage = String.join(", ", winners);
+
+        System.out.print("최종 우승자 : " + winnerMessage);
     }
 }
