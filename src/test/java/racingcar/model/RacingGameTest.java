b344860e4958feaaa008dc2291d5966b46b2ab36
Soojin6943 <sjhan0711@gmail.com>
2025-10-26 08:51:15 +0900

    test(racinggame): RacingGame 관련 테스트 코드 작성

--- src/test/java/racingcar/model/RacingGameTest.java
+++ src/test/java/racingcar/model/RacingGameTest.java
@@ -1,4 +1,37 @@
-import static org.junit.jupiter.api.Assertions.*;
+package racingcar.model;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
 class RacingGameTest {
-  
+    private static final int MOVING_FORWARD = 4;
+    private static final int STOP = 3;
+
+    @Test
+    void 랜덤_전진_테스트() {
+        Car car = new Car("pobi");
+        RacingGame racingGame = new RacingGame();
+
+        assertRandomNumberInRangeTest(
+                () -> {
+                    racingGame.attemptToMove(car);
+                    assertThat(car.getPosition()).isEqualTo(1);
+                }, MOVING_FORWARD
+        );
+    }
+
+    @Test
+    void 랜덤_멈춤_테스트() {
+        Car car = new Car("pobi");
+        RacingGame racingGame = new RacingGame();
+
+        assertRandomNumberInRangeTest(
+                () -> {
+                    racingGame.attemptToMove(car);
+                    assertThat(car.getPosition()).isEqualTo(0);
+                }, STOP
+        );
+    }
 }
