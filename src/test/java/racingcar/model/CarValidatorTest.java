9e92ec78eaa58f4c5a7e4b861d240122906a49f5
Soojin6943 <sjhan0711@gmail.com>
2025-10-26 08:48:39 +0900

    test(carvalidator): CarValidator 관련 테스트 코드 작성

--- src/test/java/racingcar/model/CarValidatorTest.java
+++ src/test/java/racingcar/model/CarValidatorTest.java
@@ -1,4 +1,22 @@
-import static org.junit.jupiter.api.Assertions.*;
+package racingcar.model;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
 class CarValidatorTest {
-  
+
+    @Test
+    void 이름_5자_이하() {
+        String carName = "javajigi";
+        assertThatThrownBy(() -> CarValidator.validateCarName(carName))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 이름_공백_예외발생() {
+        String carName = "     ";
+        assertThatThrownBy(() -> CarValidator.validateCarName(carName))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
