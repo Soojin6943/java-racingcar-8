d725ccbed6e538db00e05a870f3d2832d2c77e5e
Soojin6943 <sjhan0711@gmail.com>
2025-10-26 09:19:48 +0900

    refactor(model): 자동차 이름 입력 관련 에러 메시지 통일

--- src/main/java/racingcar/model/CarValidator.java
+++ src/main/java/racingcar/model/CarValidator.java
@@ -1,13 +1,15 @@
 package racingcar.model;
 
 public class CarValidator {
+    private static final String ERRER_MESSEGE = "자동차 이름은 1자 이상, 5자 이하만 가능합니다.";
+
     public static void validateCarName(String name) {
         if (name.length() > 5) {
-            throw new IllegalArgumentException("이름은 5자 이하만 가능합니다.");
+            throw new IllegalArgumentException(ERRER_MESSEGE);
         }
 
         if (name.isBlank()) {
-            throw new IllegalArgumentException("공백 이름은 불가능합니다");
+            throw new IllegalArgumentException(ERRER_MESSEGE);
         }
     }
 }
